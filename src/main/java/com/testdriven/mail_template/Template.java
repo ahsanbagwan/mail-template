0eb89ddc4bf3bbea11c5efab113db8a91344be6e
Ahsan <ahsanbagwan@gmail.com>
2015-05-10 20:59:24 +0530

    First commit

---
+++ src/main/java/com/testdriven/mail_template/Template.java
@@ -0,0 +1,15 @@
+package com.testdriven.mail_template;
+
+public class Template {
+
+	public Template(String templateText) {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+	public void set(String variable, String value){
+	}
+	
+	public String evaluate(){
+		return "Hello, Reader";
+	}
+}
