059868464cdbfe12fe99b242485b86489d21f2cb
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-05-11 15:40:18 +0530

    Refactored a bit

--- src/main/java/com/testdriven/mail_template/Template.java
+++ src/main/java/com/testdriven/mail_template/Template.java
@@ -1,15 +1,31 @@
 package com.testdriven.mail_template;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Template {
 
+	private Map<String, String> variables;
+	private String templateText;
+
 	public Template(String templateText) {
-		//super();
-		// TODO Auto-generated constructor stub
+		// super();
+		this.variables = new HashMap<String, String>();
+		this.templateText = templateText;
+
 	}
-	public void set(String variable, String value){
+
+	public void set(String name, String value) {
+		this.variables.put(name, value);
 	}
-	
-	public String evaluate(){
-		return "Hello, Reader";
+
+	public String evaluate() {
+		String result = templateText;
+		for (Entry<String, String> entry : variables.entrySet()){
+			String regex = "\\$\\{" + entry.getKey() + "\\}";
+			result = result.replaceAll(regex, entry.getValue());
+		}
+		return result;
 	}
 }
